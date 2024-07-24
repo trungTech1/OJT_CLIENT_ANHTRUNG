0e5e5c431e5623414e38ef23b4d9dc41ee1bda1c
unknown <quangtrungdn1994@gmail.com>
2024-07-24 09:39:51 +0700

    merge1

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,16 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { categoryActions, categoryReducer } from "./slices/category.slice";
 import { userReducer } from "./slices/user.slice";
+import { brandActions, brandReducer } from "./slices/brand.slice";
+import { configActions, configReducer } from "./slices/config.slice";
+import { colorActions, colorReducer } from "./slices/color.slice";
 
 const rootReducer = combineReducers({
   category: categoryReducer,
   userStore: userReducer,
+  brand: brandReducer,
+  config: configReducer,
+  color: colorReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
@@ -14,4 +20,7 @@ const Store = configureStore({
 });
 
 Store.dispatch(categoryActions.fecthCategories());
+Store.dispatch(brandActions.fetchBrands());
+Store.dispatch(configActions.fetchConfigs());
+Store.dispatch(colorActions.fetchColors());
 export default Store;
